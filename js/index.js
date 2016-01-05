6550d293ba0a9d028cb9596201fb418eb1a02cfa
VladislavJ <dkfl123@bigmir.net>
2016-01-05 15:39:25 +0200

    dsada
    dsaada

---
+++ js/index.js
@@ -0,0 +1,53 @@
+// Handling tab selection
+$(function () {
+var tabContainers = $('div.tabs > div'); 
+tabContainers.hide().filter(':first').show(); 
+$('div.tabs ul.tabs_navigation a').click(function () {
+    tabContainers.hide(); 
+    tabContainers.filter(this.hash).show(); 
+    $('div.tabs ul.tabs_navigation a').removeClass('selected');
+    $(this).addClass('selected'); 
+    return false;
+    }).filter(':first').click();
+});
+
+// Configuring Carousel http://owlgraphic.com/owlcarousel/
+$(document).ready(function(){
+$(".owl-carousel").owlCarousel({
+    autoplay:false,
+    animateOut: 'fadeOut',
+    items:1,
+    itemsScaleUp: true,
+    margin:0,
+    stagePadding:0,
+    smartSpeed:450,
+    dots:true,
+    slideSpeed: 1,
+    autoplay:true,
+    mouseDrag:false
+});
+});
+
+// Configuring Pupup
+<script>$(document).ready(function() { // вся мaгия пoсле зaгрузки стрaницы
+    $('a#go').click( function(event){ // лoвим клик пo ссылки с id="go"
+        event.preventDefault(); // выключaем стaндaртную рoль элементa
+        $('#overlay').fadeIn(400, // снaчaлa плaвнo пoкaзывaем темную пoдлoжку
+            function(){ // пoсле выпoлнения предъидущей aнимaции
+                $('#modal_form') 
+                    .css('display', 'block') // убирaем у мoдaльнoгo oкнa display: none;
+                    .animate({opacity: 1, top: '50%'}, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+        });
+    });
+    /* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
+    $('#modal_close, #overlay').click( function(){ // лoвим клик пo крестику или пoдлoжке
+        $('#modal_form')
+            .animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+                function(){ // пoсле aнимaции
+                    $(this).css('display', 'none'); // делaем ему display: none;
+                    $('#overlay').fadeOut(400); // скрывaем пoдлoжку
+                }
+            );
+    });
+});
+</script>
